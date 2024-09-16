c3d662c1651f4b803e62364b568f99a46a8f8f45
Karnajeet Sawant <karnajeet.sawant12@gmail.com>
2024-09-16 13:16:07 -0400

    initial pipeline test

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  project     = "erudite-azimuth-432615-c1"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "CICD-bucket"
+  location      = "US"
+  force_destroy = true
+  project = "erudite-azimuth-432615-c1"
+  public_access_prevention = "enforced"
+}
